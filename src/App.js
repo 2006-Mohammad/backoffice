c1f160687ae225a280aac43fcdf9d83e65f46814
Mohammad <ahmadism6979@gmail.com>
2025-07-28 19:30:49 +0430

    update message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import Todo from './components/Todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -14,17 +14,26 @@ function Todo() {
             setTask('');
         }
     }
+  const clearList = () =>{
+    setList([]);
+  }
+
+
   return (
-    <div>
+    <div className=' border border-3 p-3 border-success' >
+        <div >
       <h1>Task Manager</h1>
-      <input type=' text' value={task} onChange={e => setTask(e.target.value)}
-      placeholder='Enter a task'/>
-      <button onClick={addTask}>Add</button>
+      <input className='form-control' type=' text' value={task} onChange={e => setTask(e.target.value)}
+      placeholder='Enter a task'/><br/>  <br/>
+      <button className='btn btn-success ms-5' onClick={addTask}>Add</button>
+      <br/>  <br/>  <br/>
+           <button className='btn btn-success ms-5' onClick={clearList}>Add</button>
       <ul>
         {list.map((t, i) => (
             <li key={i}>{t}</li>
         ))}
       </ul>
+    </div>
     </div>
   )
 }
